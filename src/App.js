aa9d5531e7b66470a6061e7784d407b64a8842d7
murmu <mauro.mosconi@redb.ee>
2019-09-26 16:15:58 -0300

    agregado de listado de productos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import TopBar from './TopBar';
+import ProductList from './ProductList';
+import{Link, BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+  
+
+  render() {
+    return (
+      <Router> 
+      <div>
+          <TopBar></TopBar>
+          <div className="container">
+            <ProductList products = {products}></ProductList>
+          </div>
+          
+        </div>
+      </Router>
+        
+      
+    );
+  }
 }
 
-export default App;
+export default App
+
+render(<App />, document.getElementById('root'));
+
